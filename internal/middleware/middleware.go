2e485582a70bad585a706471b44c5c0bc630b514
prosergeant <sberendeyev@gmail.com>
2026-03-17 23:11:01 +0500

    fix cors

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -48,7 +48,13 @@ func Recovery(next http.Handler, logger *slog.Logger) http.Handler {
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
